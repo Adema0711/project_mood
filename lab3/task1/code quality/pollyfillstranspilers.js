03b4432d34e58167786e7523295c6c0aa0867699
Adema0711 <ademaaman0711@gmail.com>
2026-04-01 22:13:45 +0500

    Delete lab3 directory

--- lab3/task1/code quality/pollyfillstranspilers.js
+++
@@ -1,8 +0,0 @@
-if (!Math.trunc) { // if no such function
-  // implement it
-  Math.trunc = function(number) {
-    // Math.ceil and Math.floor exist even in ancient JavaScript engines
-    // they are covered later in the tutorial
-    return number < 0 ? Math.ceil(number) : Math.floor(number);
-  };
-}
