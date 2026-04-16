2e4095cb3436a7bc5a8e68e3c64f8ae1ef687ed5
A1aulym <bosajaaulym@gmail.com>
2026-04-16 11:18:16 +0500

    new fronted

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    @if (auth.isLoggedIn()) {
+      <app-navbar />
+    }
+    <router-outlet />
+  `
+})
+export class AppComponent {
+  constructor(public auth: AuthService) {}
+}
+
+
+
+
+
+
+
